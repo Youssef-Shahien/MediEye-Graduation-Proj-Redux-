95daed035535bb6825001584ee268157372ec130
Youssef Shahien <youssefshahien156@gmail.com>
2024-04-14 10:52:28 +0200

    .

--- react/src/pages/Orders/Orders.jsx
+++ react/src/pages/Orders/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import style from "./Orders.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -6,15 +6,76 @@ import {
   faSquareCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { editOrder, getOrders } from "../../store/OrderSlice";
 
 function Orders() {
+  const dispatch = useDispatch();
+  const { orders } = useSelector((state) => state.orders);
   const arrowFont = <FontAwesomeIcon icon={faChevronDown} />;
   const checkFont = <FontAwesomeIcon icon={faSquareCheck} />;
-  const [isActive, setIsActive] = useState(false);
-
-  const activeToggleHandler = (event) => {
-    setIsActive(!isActive);
+  const handleStyleWithOptionValue = (status, id) => {
+    dispatch(editOrder({ id, status }));
   };
+  //////////////////////////////////////////////////////////////////
+  const tableRowData = orders.map((item) => (
+    <tr className={`${style[`${item.status}_tr`]}`} key={item.id}>
+      <th className="text-secondary" scope="row">
+        <input className={style.my_check} type="checkbox" />
+      </th>
+      <td>{item.id}</td>
+      <td className="fw-bold">Tv 14 Inche</td>
+      <td>{item.customer}</td>
+      <td>{item.phone_number}</td>
+      {/* Start Status Board*/}
+      <td className={style.status}>
+        <div>
+          <div className="mb-3">
+            <select
+              id="exampleInputSelect"
+              className={`${style.selected_orders} ${style[`${item.status}`]}`}
+              onChange={(e) =>
+                handleStyleWithOptionValue(e.target.value, item.id)
+              }
+              value={item.status}
+            >
+              <option
+                className={`${style.new_order} text-secondary`}
+                value="new_order"
+              >
+                New Order
+              </option>
+              <option
+                className={`${style.complete} text-secondary`}
+                value="complete"
+              >
+                Complete
+              </option>
+
+              <option
+                className={`${style.rejected} text-secondary`}
+                value="rejected"
+              >
+                Rejected
+              </option>
+            </select>
+          </div>
+        </div>
+      </td>
+      {/* End Status Board*/}
+      <td className="fw-bold">{item.total_amount}$</td>
+      <td>
+        March 21,2020
+        <br />
+        00:28
+      </td>
+    </tr>
+  ));
+  //////////////////////////////////////////////////////////////////
+  // Dispatch Orders
+  useEffect(() => {
+    dispatch(getOrders());
+  }, [dispatch]);
   return (
     <div>
       <h3 className="mb-3">My Orders</h3>
@@ -64,91 +125,11 @@ function Orders() {
             <th scope="col">Customer</th>
             <th scope="col">Number</th>
             <th scope="col">Status</th>
-            <th scope="col">Created Date</th>
             <th scope="col">Price</th>
+            <th scope="col">Created Date</th>
           </tr>
         </thead>
-        <tbody className={style.table_group_divider}>
-          <tr className={style.complete_order_tr}>
-            <th className="text-secondary" scope="row">
-              <input className={style.my_check} type="checkbox" />
-            </th>
-            <td>990-123</td>
-            <td className="fw-bold">Tv 14 Inche</td>
-            <td>Youssef Shahien</td>
-            <td>01222222228</td>
-            {/* Start Status Board*/}
-            <td className={style.status}>
-              <span
-                className={`${style.complete}`}
-                onClick={activeToggleHandler}
-              >
-                Complete
-              </span>
-              <div
-                className={`${
-                  isActive
-                    ? `${style.active_status_board}`
-                    : `${style.status_board}`
-                } mt-3 bg-light rounded pt-2`}
-              >
-                <ul className="list-unstyled d-flex flex-column gap-2 justify-content-center align-items-center">
-                  <li className={`${style.complete}`}>Complete</li>
-                  <li className={`${style.new_order}`}>New Order</li>
-                  <li className={`${style.rejected}`}>Rejected</li>
-                </ul>
-              </div>
-            </td>
-            {/* End Status Board*/}
-            <td>
-              March 21,2020
-              <br />
-              00:28
-            </td>
-            <td className="fw-bold">19.0$</td>
-          </tr>
-          <tr className={style.new_order_tr}>
-            <th className="text-secondary" scope="row">
-              <input className={style.my_check} type="checkbox" />
-            </th>
-            <td>990-123</td>
-            <td className="fw-bold">Tv 14 Inche</td>
-            <td>Youssef Shahien</td>
-            <td>01222222228</td>
-            <td>
-              <span className={`${style.new_order}`}>New Order</span>
-            </td>
-            <td>
-              March 21,2020
-              <br />
-              00:28
-            </td>
-            <td className="fw-bold">19.0$</td>
-          </tr>
-          <tr className={style.rejected_order_tr}>
-            <th className="text-secondary" scope="row">
-              <input className={style.my_check} type="checkbox" />
-            </th>
-            <td>990-123</td>
-            <td className="fw-bold">Tv 14 Inche</td>
-            <td>Youssef Shahien</td>
-            <td>01222222228</td>
-            <td>
-              <span
-                className={`${style.rejected}`}
-                onClick={activeToggleHandler}
-              >
-                Rejected
-              </span>
-            </td>
-            <td>
-              March 21,2020
-              <br />
-              00:28
-            </td>
-            <td className="fw-bold">19.0$</td>
-          </tr>
-        </tbody>
+        <tbody className={style.table_group_divider}>{tableRowData}</tbody>
       </table>
       {/* End Table */}
     </div>
--- react/src/store/OrderSlice.js
+++ react/src/store/OrderSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const token =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiMzViZWM4YmIxNmIxZDVhYThlY2JlNGY5OWI5NThhNjQyNDNiMTAyMzhhMTYwNjI5YmY3NWJmNmQ4MTkwMjc2ODFjMTQ2N2M4YzdmMjBhMmMiLCJpYXQiOjE3MTIyOTAyNTEuMzAyNTE5LCJuYmYiOjE3MTIyOTAyNTEuMzAyNTIxLCJleHAiOjE3MjgxMDE0NTEuMjk0MjU2LCJzdWIiOiI2OCIsInNjb3BlcyI6W119.dv3Hh15AIEPYqXb2Fx6qQUofmc2Jst_k6QE9oKYrUkl3mjavp_TPgOl8NNU2mw1ubLaWDwhqEPF7DPiP6gCqw3FrZ9pW43Vvs1WsPetXE3TsWyMTdva-J0xyuZ6jYXmWKCC-NIHXxI92Q2yMaX2ZqqSh7EBV60_-_Ldae1Ihdzhn1pv4xPZF4-fkwPqoL_CbaF7lcSGy6w2JSOe-pyTKczyIAxfKgbqln2mHuV78M1BH00sWJ8Ke8v0ji5qdhWayr5wGVTOQyOmWjBYbUvNQpvwGefdIMpd1tw7QCGije8FAWlyHxKv-zS4zRyYRKYJUE4gBOK5NzawQZHZ7z_jBAZVWCAKFSPDVOHji9PYweZiWxpWj2i7-L2ip9YmcoPN4A4bd54nQp831gSNIQmFuzpZJ5VrJYNPXsYdhgwHxKKWD5-fDmTC4jv2yfEtiSVBaeN68a08AV6fTd5WMyLAe9SklnGz2v0b4pfXNeCIvWZwMAblIjhztasixmAmByYrNJ3iZ31IXtZI8mMJtYrJ2GdR9HGsNPMW33_mb_NPVYIDrj0r2WSqsY354ySI8OA5i_iLN8f5J0rpr7vEHOV4YeH3gp06w3GcW6OO8ZrdMCxKGHfQTTUuuVjCmnPcAqiX4nYl-aj99SnEdM1AskjtuIGPFif0245QrKQ7YXgTPWHM";
 const baseURL = `https://7d4f-154-237-97-232.ngrok-free.app/api`;
-
+//////////////// GetOrders Action //////////////
 export const getOrders = createAsyncThunk(
   "orders/getOrders",
   async (_, thunkAPI) => {
@@ -16,12 +16,40 @@ export const getOrders = createAsyncThunk(
         },
       });
       const data = await res.json();
+      console.log(data);
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
     }
   }
 );
+//////////////// EditOrders Action //////////////
+
+export const editOrder = createAsyncThunk(
+  "products/editProduct",
+  async (orderData, thunkAPI) => {
+    const { rejectWithValue, dispatch } = thunkAPI;
+    console.log(orderData);
+    console.log(orderData.id);
+    try {
+      const res = await fetch(`${baseURL}/order/${orderData.id}`, {
+        method: "POST",
+        body: JSON.stringify(orderData),
+        headers: {
+          "content-type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ${token}`,
+          "ngrok-skip-browser-warning": "true",
+        },
+      });
+      const data = await res.json();
+      dispatch(getOrders());
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+//////////////////// createSlice Reducer ///////////////
 
 const orderSlice = createSlice({
   name: "orders",
@@ -35,9 +63,21 @@ const orderSlice = createSlice({
       })
       .addCase(getOrders.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.products = action.payload.products;
+        state.orders = action.payload.orders;
       })
       .addCase(getOrders.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      // EditOrder
+      .addCase(editOrder.pending, (state, action) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(editOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      .addCase(editOrder.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
       });
